e8bbd57287933ab7ed67a6e9c8daa059d283a291
Personal <Personal@Familia>
2021-02-23 20:51:03 -0600

    Imprimiendo nombre del alumno.

---
+++ src/gitgrupoa59538/GitGrupoA59538.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitgrupoa59538;
+
+/**
+ *
+ * @author Personal
+ */
+public class GitGrupoA59538 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Jossmar Oswaldo Ku Cupul");
+    }
+    
+}
